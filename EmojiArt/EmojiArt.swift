baf9569834cfc61dc2f45394f691c306557e2f8b
fbara <fbara1@yahoo.com>
2020-11-27 19:50:33 -0600

    End of video 7.

---
+++ EmojiArt/EmojiArt.swift
@@ -0,0 +1,38 @@
+//
+//  EmojiArt.swift
+//  EmojiArt
+//
+//  Created by Frank Bara on 11/14/20.
+//  Copyright © 2020 BaraLabs. All rights reserved.
+//
+//  EmojiArt model
+
+import Foundation
+
+struct EmojiArt {
+    var backgroundURL: URL?
+    var emojis = [Emoji]()
+    
+    struct Emoji: Identifiable {
+        let text: String
+        var x: Int //offset from center
+        var y: Int //offset from center
+        var size: Int
+        let id: Int
+        
+        fileprivate init(text: String, x: Int, y: Int, size: Int, id: Int) {
+            self.text = text
+            self.x = x
+            self.y = y
+            self.size = size
+            self.id = id
+        }
+    }
+    
+    private var uniqueEmojiID = 0
+    
+    mutating func addEmoji(_ text: String, x: Int, y: Int, size: Int) {
+        uniqueEmojiID += 1
+        emojis.append(Emoji(text: text, x: x, y: y, size: size, id: uniqueEmojiID))
+    }
+}
